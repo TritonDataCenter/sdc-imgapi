2df498e1c3a11af16c23356a461b894a987fd163
Trent Mick <trentm@gmail.com>
2012-11-21 16:11:07 -0800

    IMGAPI-51 imgapi test suite is blowing imgapi zone mem cap (support for running the test suite from the GZ)

--- test/rm-test-data.sh
+++ test/rm-test-data.sh
@@ -65,10 +65,15 @@ if [[ -n "$opt_local" ]]; then
     rm -rf $(json storage.local.dir <$CFG_FILE)
 else
     # Luke's created datasets.
-    dns=$($TOP/test/sdc-ldap s -b 'ou=images, o=smartdc' '(&(objectclass=sdcimage)(owner=91ba0e64-2547-11e2-a972-df579e5fddb3))' \
+    dns=$($TOP/test/sdc-ldap s -b 'ou=images, o=smartdc' \
+        '(&(objectclass=sdcimage)(owner=91ba0e64-2547-11e2-a972-df579e5fddb3))' \
         | (grep '^dn' || true) | cut -d' ' -f2- | sed 's/, /,/g' | xargs)
     # All the test-data.ldif dns.
     dns+=" $(grep '^dn' $TOP/test/test-data.ldif | cut -d' ' -f2- | sed 's/, /,/g' | xargs)"
+    # Some more created as part of the test suite.
+    dns+=" uuid=c58161c0-2547-11e2-a75e-9fdca1940570,ou=images,o=smartdc"
+    dns+=" uuid=47e6af92-daf0-11e0-ac11-473ca1173ab0,ou=images,o=smartdc"
+    dns+=" uuid=1fc068b0-13b0-11e2-9f4e-2f3f6a96d9bc,ou=images,o=smartdc"
 
     for dn in $dns; do
         if [[ -n "$($TOP/test/sdc-ldap search -b "$dn")" ]]; then
@@ -77,3 +82,6 @@ else
         fi
     done
 fi
+
+rm -rf /var/tmp/images-test-file-*
+rm -rf /var/tmp/imgapi-test-file-*
