a07313885f36a514a980d62d4c7315619837f8bd
Trent Mick <trentm@gmail.com>
2013-08-25 21:42:39 -0700

    IMAGE-318 us-beta-4 seed package data, defensive script

--- tools/seed-packages/install-seed-packages-us-beta-4.sh
+++ tools/seed-packages/install-seed-packages-us-beta-4.sh
@@ -2,11 +2,12 @@
 
 TOP=$(cd $(dirname $0)/ >/dev/null; pwd)
 
-UUID1=92e2b20a-0c37-11e3-9605-63a778146273
-UUID2=9c1948c0-0c37-11e3-be34-5780f9789210
-UUID3=a3501ccc-0c37-11e3-965d-ef7e825515c9
-UUID4=b1575678-0c37-11e3-8a27-63052c0a42c1
-NETWORKS='["9ec60129-9034-47b4-b111-3026f9b1a10f", "5983940e-58a5-4543-b732-c689b1fe4c08"]'
+UUID1=d6cbefc0-8678-3b45-afd2-ce5864249c25
+UUID2=8c3710d8-bf7e-e54d-9452-60cce2a0fa70
+UUID3=dc0c1879-349b-8a45-902a-0097e23ec98b
+UUID4=147ba2ea-a518-7f49-8f75-041243789044
+# 'external' network on us-beta-4
+NETWORKS='["213fb868-f613-46df-90db-b85c60ffaa05"]'
 
 if [[ ! -f $(TOP)/seed.ldif.in ]]; then
     echo "$0: fatal error: '$(TOP)/seed.ldif.in' does not exist" >&2
